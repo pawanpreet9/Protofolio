5685eaa67be35af4ebd7cab3ecbe0b9b6d155865
Pawanpreet Kaur <pawanpreetkaur4@rrc.ca>
2023-08-08 23:00:27 -0700

    first commit

---
+++ src/components/Skills.js
@@ -0,0 +1,35 @@
+// src/components/Skills.js
+
+import { BadgeCheckIcon, ChipIcon } from "@heroicons/react/solid";
+import React from "react";
+import { skills } from "../data";
+
+export default function Skills() {
+  return (
+    <section id="skills">
+      <div className="container px-5 py-10 mx-auto">
+        <div className="text-center mb-20">
+          <ChipIcon className="w-10 inline-block mb-4" />
+          <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
+            Skills &amp; Technologies
+          </h1>
+          <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
+          Welcome to my skills page, where I'm excited to share the toolbox that empowers me to bring innovative web solutions to life. From front-end to back-end, I've cultivated a versatile skill set that enables me to tackle a wide range of challenges and create exceptional user experiences.
+          </p>
+        </div>
+        <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+          {skills.map((skill) => (
+            <div key={skill} className="p-2 sm:w-1/2 w-full">
+              <div className="bg-gray-800 rounded flex p-4 h-full items-center">
+                <BadgeCheckIcon className="text-green-400 w-6 h-6 flex-shrink-0 mr-4" />
+                <span className="title-font font-medium text-white">
+                  {skill}
+                </span>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,41 @@
+export const projects = [
+  {
+    title: "Ebook store Ralis project",
+    subtitle: "Ruby rails",
+    description:
+      "With the use of ruby on rails create a bookstore that show the all books and data. Add the database concept",
+    image: "./project2.jpeg",
+    link: "https://github.com/pawanpreet9/EBookStore.git",
+  },
+  {
+    title: "Ecommerce website",
+    subtitle: "Ruby on rails, php, html, css and javascript",
+    description:
+      "bookshelf_oasis books e commerece website",
+    image: "./project1.jpeg",
+    link: "https://github.com/pawanpreet9/Bookshelf_oasis_app.git",
+  }
+];
+
+export const testimonials = [
+  {
+    quote:
+    "It's been a pleasure mentoring Pawanpreet, a dedicated web development student. She consistently demonstrates a strong grasp of coding principles and a knack for translating ideas into functional designs." ,
+
+    name: "Mentor",
+    company: "Red River College Polytechnic",
+  }
+];
+
+export const skills = [
+ "HTML5",
+ " CSS3",
+ "JavaScript",
+ "Databases",
+ "API Integration",
+
+ "UI/UX Design",
+ "Testing and Debugging",
+ "PHP, Ruby On Rails"
+
+];
